3b13a933ec33a1b1ab9962ce38d6bddcc02758d5
ee7klt <kltah1@gmail.com>
2016-05-24 13:38:36 +0800

    added card for essay

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 var mail = document.getElementById('mail');
-var contactform = $("contactform");
-contactform.validate();
+var contactform = document.getElementById('contactform');
+
 //var msg = document.getElementById('status');
 mail.addEventListener("keyup", function () {
   // Each time the user types something, we check if the
@@ -12,13 +12,31 @@ $(function() {
 
 
   $('#contactform input').keyup(function() {
-    window.console.log(contactform);
-    if(contactform.valid()) {
+    window.console.log(contactform.checkValidity());
+    if(contactform.checkValidity()) {
       $('#next').prop('disabled',false);
     }
     else {
       $('#next').prop('disabled',true);
     }
+  });
+
+  $('#next').click(function() {
+    window.console.log('submit button pressed');
+    $('#mycard').hide();
+  });
+
+
+  $('#essaytext').keyup(function() {
+     var words = this.value.match(/\S+/g).length;
+     window.console.log(words);
+     if (words > 50) {
+       $('#nextessay').prop('disabled', false);
+     } else {
+       $('#nextessay').prop('disabled', true);
+     }
+
+
   });
 
 });
