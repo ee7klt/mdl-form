5c1a6a662f656aa3ccc29d7d6eaa00c7cf65fda8
ee7klt <kltah1@gmail.com>
2016-05-30 11:35:37 +0800

    resume upload design and logic
    * need to fix: now the disabled button is transparent again.
    * next step: show word count at bottom of essay card
    * next step: gather cards in to carousel
    * next step: animate carousel

--- script.js
+++ script.js
@@ -1,12 +1,40 @@
 var mail = document.getElementById('mail');
 var contactform = document.getElementById('contactform');
-
+var uploadButton = document.getElementById('resume-upload');
+var resumeUpload = document.getElementById('resume-upload');
+var fileInputText = document.getElementById('file_input_text');
+var fileInputTextDiv = document.getElementById('file-input-text-div');
+var nextFile = document.getElementById('nextfile');
 //var msg = document.getElementById('status');
 mail.addEventListener("keyup", function () {
   // Each time the user types something, we check if the
   // email field is valid.
   //window.console.log('email is valid? ' +mail.validity.valid);
 }, false);
+
+resumeUpload.addEventListener('change', function() {
+  console.log('file input changed');
+
+  var fullPath = resumeUpload.value;
+  if (fullPath) {
+    var startIndex = (fullPath.indexOf('\\') >= 0 ? fullPath.lastIndexOf('\\') : fullPath.lastIndexOf('/'));
+    var filename = fullPath.substring(startIndex);
+    if (filename.indexOf('\\') === 0 || filename.indexOf('/') === 0) {
+      filename = filename.substring(1);
+    }
+
+  }
+  fileInputText.value = (typeof filename === 'undefined') ? '': filename;
+  if (fileInputText.value.length != 0) {
+    fileInputTextDiv.classList.add('is-focused');
+    nextFile.prop('disabled',false);
+  } else {
+    fileInputTextDiv.classList.remove('is-focused');
+    nextFile.prop('disabled',true);
+  }
+})
+
+
 
 $(function() {
 
@@ -23,20 +51,23 @@ $(function() {
 
   $('#next').click(function() {
     window.console.log('submit button pressed');
-    $('#mycard').hide();
+    // $('#mycard').hide();
   });
 
 
   $('#essaytext').keyup(function() {
-     var words = this.value.match(/\S+/g).length;
-     window.console.log(words);
-     if (words > 50) {
-       $('#nextessay').prop('disabled', false);
-     } else {
-       $('#nextessay').prop('disabled', true);
-     }
+    var words = this.value.match(/\S+/g).length;
+    window.console.log(words);
+    if (words > 50) {
+      $('#nextessay').prop('disabled', false);
+    } else {
+      $('#nextessay').prop('disabled', true);
+    }
 
 
   });
+
+
+
 
 });
